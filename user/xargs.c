d790f7f610f7ab0984ef769fe331ea63fc04da29
aakKel <952057534@qq.com>
2022-10-06 22:34:33 +0800

    l w

--- user/xargs.c
+++ user/xargs.c
@@ -6,30 +6,29 @@
 int main(int argc, char *argv[])
 {
     char *command = argv[1];
-    char bf;
     char param[MAXARG][MAX_LEN];
     char *m[MAXARG];
+    char bf ;
 
     while(1)
     {
-        cnt == argv.size()
         int cnt = argc - 1;
-        memset(param,0,MAX_LEN *MAXARG);
-        for(int i = 1;i < argc ;++i)
+        memset(param,0,MAXARG * MAX_LEN);
+        for(int i = 1;i < argc; ++i)
+        {
             strcpy(param[i - 1],argv[i]);
+        }
 
         int cursor = 0;
         int flag = 0;
         int read_res ;
-        //read from pipe or ...
         while((read_res = read(0,&bf,1)) > 0 && bf != '\n')
         {
             if(bf == ' ' && flag == 1)
             {
-                //add to the last position
+                flag = 0;
                 cnt ++;
                 cursor = 0;
-                flag = 0;
             }
             else if(bf != ' ')
             {
@@ -37,18 +36,20 @@ int main(int argc, char *argv[])
                 flag = 1;
             }
         }
-        // bf == '\n'
-        if(read_res <= 0)
-            break;
+        if(read_res <= 0) break;
+
         for(int i = 0;i < MAXARG - 1; ++i)
             m[i] = param[i];
+
         m[MAXARG - 1] = 0;
+
         if(fork() == 0)
         {
             exec(command,m);
             exit(0);
         }
         else wait((int*)0);
+
     }
     exit(0);
 
